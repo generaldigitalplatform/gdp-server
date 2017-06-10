fd8666bf7b651c1630bfb04036d473f532378a60
gdmp <manikandan.muthuv@gmail.com>
2017-06-10 19:58:49 +0530

    addee

--- api/controllers/CustomerFeedbackController.js
+++ api/controllers/CustomerFeedbackController.js
@@ -29,8 +29,8 @@ exports.findCustomerFeedbackById = function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-	var customerFeedbackObjId = new ObjectId(req.params.Id);
-	var query = {$or:[{"CustomerId":req.params.Id},{"_id":customerFeedbackObjId},{"PrimaryPhone":req.params.Id},{"SecondaryPhone":req.params.Id},{'ContactAddress.Pincode':req.params.Id},{'ContactAddress.City':req.params.Id},{'ContactAddress.Zone':req.params.Id},{'ContactAddress.State':req.params.Id},{'ContactAddress.Area':req.params.Id}]};
+	var customerFeedbackObjId = new ObjectId((req.params.Id.length < 12) ? "123456789012" : req.params.Id);
+	var query = {$or:[{"CustomerId":customerFeedbackObjId},{"_id":customerFeedbackObjId},{"PrimaryPhone":req.params.Id},{"SecondaryPhone":req.params.Id},{'ContactAddress.Pincode':req.params.Id},{'ContactAddress.City':req.params.Id},{'ContactAddress.Zone':req.params.Id},{'ContactAddress.State':req.params.Id},{'ContactAddress.Area':req.params.Id}]};
 	
 	customerFeedbackModel.findOne(query,function(err,feedback){
 		if (err) res.send(err);;
@@ -47,7 +47,7 @@ exports.updateCustomerFeedbackById = function(req,res){
 	var updateData = req.body;
 	var options = {upsert:true,new: false};
 	
-	var customerFeedbackObjId = new ObjectId(req.params.Id);
+	var customerFeedbackObjId = new ObjectId((req.params.Id.length < 12) ? "123456789012" : req.params.Id);
 	var query = {$or:[{"_id":req.params.Id},{"PrimaryPhone":req.params.Id},{"SecondaryPhone":req.params.Id},{'ContactAddress.Pincode':req.params.Id},{'ContactAddress.City':req.params.Id},{'ContactAddress.Zone':req.params.Id},{'ContactAddress.State':req.params.Id},{'ContactAddress.Area':req.params.Id}]};
 	
 	customerFeedbackModel.findOneAndUpdate(query,{$add:updateData},options,function(err,feedback){
--- api/models/EmployeeLocationModel.js
+++ api/models/EmployeeLocationModel.js
@@ -1,16 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var EmployeeLocationSchema = new Schema({
 	EmployeeId:Object,
 	TrackDateTime:Date,
 	Status:String,
-	CurrentLocation:[{	
-	  			Name: { type: String },		  
-				coordinates:[Number]				
-			}]	
+	LocationName:String,
+	geo: {
+	    type: {
+	      type: "String",
+	      required: true,
+	      enum: ['Point', 'LineString', 'Polygon'],
+	      default: 'Point'
+	    },
+    coordinates: {
+        type: [Number]
+    }
+}
+
 },{"strict":false});
 
-EmployeeLocationSchema.index({ "Location": "2dsphere" });
-
+EmployeeLocationSchema.index({ 'geo': '2dsphere' });
 module.exports = mongoose.model('EmployeeLocation',EmployeeLocationSchema);
