5d768b1a7ef0dd869601cf40dec84bedc8e2ad5b
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-08-26 23:40:26 +0530

    'aaded'

--- api/config/database.js
+++ api/config/database.js
@@ -1,4 +1,10 @@
 module.exports = {
-	'databaseUri':'mongodb://localhost:27017/gdp'
-	//'databaseUri' = 'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1'
+	'databaseUri':'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1',
+	'login':'https://gdp-test-server.herokuapp.com/api/auth/login/',
+	'register':'https://gdp-test-server.herokuapp.com/api/auth/register/',
+	'protected':'https://gdp-test-server.herokuapp.com/api/auth/protected/',
+	'customerprofile':'https://gdp-test-server.herokuapp.com/api/customer/profile/',
+	'customerfeedback':'https://gdp-test-server.herokuapp.com/api/customer/feedback/',
+	'job':'https://gdp-test-server.herokuapp.com/api/employee/job/',
+	'registerfieldforce':'https://gdp-test-server.herokuapp.com/api/employee/fieldforce/',
 }
--- api/controllers/googleMapsController.js
+++ api/controllers/googleMapsController.js
@@ -1,23 +1,55 @@
 var request = require('request'),
-	googleMapsClient = require('@google/maps').createClient({
-	key:'AIzaSyBh-Su_ZydI-kofZ0incHVCHPECZytqEK0'
-});
+	db = require('../config/database');
 
 //var serverkey = 'AAAAUGGsxGs:APA91bF1qVPPcbsSvYAbtcJzslTVFUEk3hpZOJWwbR_Rc8MBDZXpH8Bxf4Rn-SWXX4TxpMGF-3YWHDNC97i-wIxC4qPDq_htpsNr-eKTjOMKf7jftuKQD_nTOc_ZVIxNg7KscviAZUj8';  
 
 exports.findGeoLocation = function(req,res){
-	// googleMapsClient.geocode({"Address":"1600 Amphitheatre Parkway, Mountain View, CA"},function(err, response){
-	// 	if(err) console.log(err);
-	// 	console.log(response.json.results);
-	// });
-	latlang = '11.4118144,76.7125018';
+
+    
+	var lat = req.body.geo.lat; 
+	var lng = req.body.geo.lng;
+	var action = req.body.action;
+	var objectId = req.body.objectId;
+	var actionData;
+
+
 	var options = {     
-        uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latlang,
+        uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat + ',' + lng,
         method: 'POST'    
     }
     request(options,function(error, response, body){
     	if(error) return error;
     	var Data = JSON.parse(body);
     	var address = Data.results[0].formatted_address;
+
+    	if(action==='started'){
+	        	actionData = {
+	        		"StartedLocation" :{
+						"DateTime": new Date().toISOString(),
+						"Area": address,
+						"Coordinates": [lat,lng]							
+	        	}
+	        }
+	    } else if(action === 'reached'){
+	    	actionData = {
+	        		"ReachedLocation" :{
+						"DateTime": new Date().toISOString(),
+						"Area": address,
+						"Coordinates": [lat,lng]							
+	        	}
+	        }
+	    }      
+    	var putStartJob = {     
+        uri:db.job + objectId,
+        method: 'PUT',
+        form:actionData,
+        headers: {'Content-Type': 'application/json',"Authorization": req.headers.authorization}   
+    	}  
+
+        request(putStartJob,function(error, response, body){
+			if(error) return error;
+			res.json(response);
+        });   
+
     });
 };
