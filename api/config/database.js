ce8a756319586fa801ab1c2cffce5dfd88a61544
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-09-15 11:26:37 +0530

    'added'

--- api/config/database.js
+++ api/config/database.js
@@ -1,11 +1,4 @@
 module.exports = {
 	'databaseUri':'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1',
-	'googlemapsapi':'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
-	'login':'https://gdp-test-server.herokuapp.com/api/auth/login/',
-	'register':'https://gdp-test-server.herokuapp.com/api/auth/register/',
-	'protected':'https://gdp-test-server.herokuapp.com/api/auth/protected/',
-	'customerprofile':'https://gdp-test-server.herokuapp.com/api/customer/profile/',
-	'customerfeedback':'https://gdp-test-server.herokuapp.com/api/customer/feedback/',
-	'job':'https://gdp-test-server.herokuapp.com/api/employee/job/',
-	'registerfieldforce':'https://gdp-test-server.herokuapp.com/api/employee/fieldforce/',
+	'googlemapsapi':'https://maps.googleapis.com/maps/api/geocode/json?latlng='
 }
--- server.js
+++ server.js
@@ -3,20 +3,8 @@ var express 	= require('express'),
 	mongoose 	= require('mongoose'),
 	bodyParser  = require('body-parser'),
 	cors = require("cors"),
-	compression = require('compression'),
-	//databaseUri = 'mongodb://127.0.0.1:27017/gdp', //process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gdp',
-	databaseUri = 'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1';
-	// customerProfileModel = require('./api/models/customerProfileModel'),
-	// productUsageModel = require('./api/models/ProductUsageModel'),
-	// employeeProfileModel = require('./api/models/EmployeeProfileModel'),
-	//jobModel = require('./api/models/JobModel');
-	// employeeLocationModel = require('./api/models/EmployeeLocationModel');
-	// customerFeedbackModel = require('./api/models/CustomerFeedbackModel');
-	// fieldForceModel = require('./api/models/fieldforceModel'),
-	// deviceTokenModel = require('./api/models/DeviceTokenModel');
-	// pushMessageModel = require('./api/models/PushMessageModel');
-	// userModel = require('./api/models/User');
-	// employeeModel = require('./api/models/Employee');
+	db = require('./api/config/database');
+	// databaseUri = 'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1';
 
 
 
@@ -25,16 +13,10 @@ app.options('*', cors());
 var connectionOptions = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } }; 
 
-mongoose.connect(databaseUri,connectionOptions);
+mongoose.connect(db.databaseUri,connectionOptions);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(compression());
-
-// var customerProfileroutes = require('./api/routes/customerProfileRoute');
-// var	employeeProfileroutes= require('./api/routes/EmployeeProfileRoute');
-// var deviceTokenRoute = require('./api/routes/DeviceTokenRoute');
-// var userRoute = require('./api/routes/userRoute');
 
 var employerRoute = require('./api/routes/employerRoute');
 var employeeRoute = require('./api/routes/employeeRoute');
@@ -42,12 +24,6 @@ var productOwnerRoute = require('./api/routes/productOwnerRoute');
 var jobRoute = require('./api/routes/jobRoute');
 var pushMessageRoute = require('./api/routes/PushMessageRoute');
 var googleMapsRoute = require('./api/routes/googleMaps');
-
-
-//var routers = require('./api/routes');
-
-// app.use('/employee',employeeProfileroutes);
-// app.use('/customer',customerProfileroutes);
 
 var port = 3000;
 var httpServer = require('http').createServer(app);
@@ -63,11 +39,4 @@ jobRoute(app);
 pushMessageRoute(app);
 googleMapsRoute(app);
 
-//deviceTokenRoute(app);
-//userRoute(app);
-
-
-
-
-//routers(app);
 module.exports = app;
