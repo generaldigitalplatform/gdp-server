d5f1ae00a8e83929e0910bfff7c9a00e526ff751
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-09-16 13:20:14 +0530

    'added'

--- api/controllers/googleMapsController.js
+++ api/controllers/googleMapsController.js
@@ -1,6 +1,7 @@
 var request = require('request'),
 	reqprom = require('request-promise'),
-	db = require('../config/database');
+	db = require('../config/database'),
+	NodeGeocoder = require('node-geocoder');
 
 exports.findGeoLocation = function(req,res){
     
@@ -16,17 +17,32 @@ exports.findGeoLocation = function(req,res){
 
 	var putStartJob = {};
 
-	var options = {     
-        uri:db.googlemapsapi + lat + ',' + lng,
-        method: 'POST',
-        json:true    
-    };
-    reqprom(options)
-    	.then(function(response){
+	var payload = {
+		  provider: 'google',
+		  // Optional depending on the providers
+		  httpAdapter: 'https', // Default
+		  apiKey: db.googlemapsapikey, // for Mapquest, OpenCage, Google Premier
+		  formatter: null         // 'gpx', 'string', ...
+	};
 
-			if(response.results.length > 0){
+	var geocoder = NodeGeocoder(payload);
 
-	    	var address = response.results[0].formatted_address;
+	geocoder.reverse({lat:45.767, lon:4.833}, function(err, response) {
+	// });
+
+
+	// var options = {     
+ //        uri:db.googlemapsapi + lat + ',' + lng,
+ //        method: 'POST',
+ //        json:true    
+ //    };
+
+ //    reqprom(options)
+ //    	.then(function(response){
+
+			if(response.length > 0){
+
+	    	var address = response[0].formattedAddress;
 
 	    	if(action==='started'){
 		        	actionData = {
